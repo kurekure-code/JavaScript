cd9662982d4cbe619be735c442438e71ceacb640
hoge <s03281@mcpc-0215.cyberagent.co.jp>
2018-08-07 17:43:16 +0900

    lesson1

---
+++ JavaScript_Tutorials_for_Beginners/lesson_1/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>JS lesson1</title>
+    </head>
+    <body>
+        <h1>JavaScript lesson1</h1>
+
+        <script type="text/javascript">
+            alert("Hi JavaScript");
+        </script>
+    </body>
+</html>
